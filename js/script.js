7d0891bb72290edf90b76652a9f8e3ff8bd03ace
alinagolos <goloschepovaalina@gmail.com>
2023-09-09 22:41:46 +0400

    feat:add section destination services

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.services__swiper', {});
+
+var swiper = new Swiper('.destination__swiper', {
+  slidesPerView: 3,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: '.destination__button-next',
+    prevEl: '.destination__button-prev',
+  },
+});
