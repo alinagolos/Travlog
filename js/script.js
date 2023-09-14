b0a469305349dfb59dc6f56fec83f0dd4edaca9e
alinagolos <goloschepovaalina@gmail.com>
2023-09-14 15:37:01 +0400

    feat:add new section footer

--- js/script.js
+++ js/script.js
@@ -8,3 +8,17 @@ var swiper = new Swiper('.destination__swiper', {
     prevEl: '.destination__button-prev',
   },
 });
+
+var swiper = new Swiper('.reviews__swiper', {
+  cssMode: true,
+  navigation: {
+    prevEl: '.reviews__button-prev',
+    nextEl: '.reviews__button-next',
+  },
+  pagination: {
+    el: '.reviews-pagination',
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
